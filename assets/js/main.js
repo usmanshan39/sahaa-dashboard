7483d8772d84bdde07b587ac133cf0ca8a35bdb8
usmanshan39 <70626124+usmanshan39@users.noreply.github.com>
2022-07-19 14:03:50 +0500

    add line chart

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,3 +76,73 @@ const myChart = new Chart(
   document.getElementById('myChart'),
   config
 );
+
+
+/*=========================================
+Analytic activity Chart
+===========================================*/
+var activity = document.getElementById('activity');
+
+var chart = new Chart(activity, {
+    // The type of chart we want to create
+    type: 'line',
+    
+    // The data for our dataset
+    data: {
+        labels: ["May 12, 2022", "May 15, 2022", 'Aug 20, 2022'],
+        datasets: [
+        {
+          label: "",
+          backgroundColor: 'transparent',
+          borderColor: '#2479FF',
+          data: [0, 65, 52, 115, 98, 165, 125],
+          lineTension: 0,
+          pointRadius: 4,
+          pointBackgroundColor: 'rgba(255,255,255,1)',
+          pointHoverBackgroundColor: 'rgba(255,255,255,0.6)',
+          pointHoverRadius: 8,
+          pointHitRadius: 30,
+          pointBorderWidth: 2,
+          pointStyle: 'rectRounded'
+        },
+          {
+          label: "",
+          backgroundColor: 'transparent',
+          borderColor: '#FA254C',
+          data: [45, 38, 100, 87, 152, 187, 85],
+          lineTension: 0,
+          pointRadius: 4,
+          pointBackgroundColor: 'rgba(255,255,255,0)',
+          pointHoverBackgroundColor: 'rgba(255,255,255,0.6)',
+          pointHoverRadius: 8,
+          pointHitRadius: 30,
+          pointBorderWidth: .3,
+          pointStyle: 'rectRounded'
+        }
+      ]
+    },
+    
+    // Configuration options go here
+    options: {
+      legend: {
+         display: false
+       },
+      scales: {
+        xAxes: [{
+          gridLines: {
+            display:false
+          }
+        }],
+        yAxes: [{
+          gridLines: {
+             display:true
+          },
+       ticks: {
+           
+          },
+       }]
+     },
+     tooltips: {
+    }
+  }
+});
