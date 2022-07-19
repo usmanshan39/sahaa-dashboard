470c697cbfc9b61b4e547ea6f74bf9be4b117203
usmanshan39 <70626124+usmanshan39@users.noreply.github.com>
2022-07-19 12:33:52 +0500

    add-pdf

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,26 @@ $buttonb.addEventListener('click', (e) => {
   e.preventDefault();
   $wrapper.classList.toggle('toggled');
 });
+
+
+
+// bar graph
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["M", "T", "W", "T", "F", "S", "S"],
+    datasets: [{
+      label: 'apples',
+      data: [5000, 100, 500],
+      backgroundColor: "rgba(153,255,51,1)"
+    }, {
+      label: 'oranges',
+      data: [4000, 3500, 400],
+      backgroundColor: "rgba(255,153,0,1)"
+    }]
+  }
+});
+
+
+
